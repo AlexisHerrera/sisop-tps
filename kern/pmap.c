eb0169d139aa5234e28ac4ff0f553cba489f9f39
Alexis Herrera <alexysaguilar@gmail.com>
2021-10-29 20:13:50 -0300

    page init con rangos correctos iniciales

--- kern/pmap.c
+++ kern/pmap.c
@@ -266,11 +266,16 @@ page_init(void)
 	// Change the code to reflect this.
 	// NB: DO NOT actually touch the physical memory corresponding to
 	// free pages!
-	size_t i;
-	for (i = 0; i < npages; i++) {
-		pages[i].pp_ref = 0;
-		pages[i].pp_link = page_free_list;
-		page_free_list = &pages[i];
+	physaddr_t page_address;
+	for (size_t i = 1; i < npages; i++) { // Ignoro pagina 0
+		page_address = i*PGSIZE;
+		// Si está en el rango PGSIZE hasta IOPHYSEM es válida la memoria
+		bool is_free_page = page_address >= PADDR(boot_alloc(0)) || page_address < IOPHYSMEM;
+		if (is_free_page) { 
+			pages[i].pp_ref = 0;
+			pages[i].pp_link = page_free_list;
+			page_free_list = &pages[i];
+		}
 	}
 }
 
