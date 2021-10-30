248842539a4062e5d9648ed705ede75c4ef3308c
Alexis Herrera <alexysaguilar@gmail.com>
2021-10-30 11:21:13 -0300

    free implemented

--- kern/pmap.c
+++ kern/pmap.c
@@ -326,6 +326,11 @@ page_free(struct PageInfo *pp)
 	// Fill this function in
 	// Hint: You may want to panic if pp->pp_ref is nonzero or
 	// pp->pp_link is not NULL.
+	if (pp->pp_ref!=0 || pp->pp_link) {
+		panic("page free failed");
+	}
+	pp->pp_link = page_free_list;
+	page_free_list = pp;
 }
 
 //
