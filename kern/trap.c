2e55c48d635a87730ba3290d9a2d9ee60c7a7d1a
TheGrey <fvazquezf@fi.uba.ar>
2021-11-17 20:42:48 -0300

    cambio del traphandler para soluciuonar el bug feat patricio

--- kern/trap.c
+++ kern/trap.c
@@ -221,7 +221,7 @@ trap_dispatch(struct Trapframe *tf)
 		a5 = tf->tf_regs.reg_esi;
 		result = syscall(num, a1, a2, a3, a4, a5);
 		tf->tf_regs.reg_eax = result;
-		break;
+		return;
 	}
 
 	// Unexpected trap: The user process or the kernel has a bug.
