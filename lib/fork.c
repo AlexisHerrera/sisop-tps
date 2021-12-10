bb2fca3d306de26be9287af9f13b1c393ab54845
Alexis Herrera <alexysaguilar@gmail.com>
2021-12-10 18:39:02 -0300

    Duppage el remapeo se hace despues del mapeo a destino, porque sino falla

--- lib/fork.c
+++ lib/fork.c
@@ -83,16 +83,21 @@ duppage(envid_t envid, unsigned pn)
 	// Permisos del padre menos PTE_W
 	int par_perm = pte & 0xFFF;
 	int perm = par_perm & (~PTE_W);
-
-	if ((par_perm & PTE_W) || (par_perm & PTE_COW)) {
+	// Si tiene marcado el permiso de escritura
+	// se mapea con el flag PTE_COW
+	if ((par_perm & PTE_W)) {
 		perm |= PTE_COW;
+	}
+	if ((r = sys_page_map(0, (void *) addr, envid, (void *) addr, perm)) < 0) {
+		panic("sys_page_map: %e", r);
+	}
+	// Si el destino tiene marcado el PTE_COW
+	// remapeo la dirección con PTE_COW
+	if (perm & PTE_COW) {
 		if ((r = sys_page_map(0, (void *) addr, 0, (void *) addr, perm)) <
 		    0) {
 			panic("sys_page_map: %e", r);
 		}
-	}
-	if ((r = sys_page_map(0, (void *) addr, envid, (void *) addr, perm)) < 0) {
-		panic("sys_page_map: %e", r);
 	}
 	return 0;
 }
